9b39d40e68ece2771b9a44f5be7b001226f6ffe4
Frank Robnik <frank.robnik@gmail.com>
2021-11-09 16:39:45 +0100

    feat: add react-paper theme

---
+++ utils/foresterPaperTheme.util.ts
@@ -0,0 +1,11 @@
+import { DefaultTheme } from 'react-native-paper';
+
+export const foresterPaperTheme = {
+  ...DefaultTheme,
+  colors: {
+    ...DefaultTheme.colors,
+    primary: '#8DC027',
+    accent: '#C09B30',
+    background: '#f6f6f6',
+  },
+};
